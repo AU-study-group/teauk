6e7d5384b565c9e91e6b8781520bfe7d2107a051
teauk03 <kto031124@gamil.com>
2023-05-13 18:57:49 +0900

    나의 식당에 온걸 환영해 연진아 weak 1

---
+++ weak1/main.js
@@ -0,0 +1,27 @@
+export const menu = `
+<h1>나의 식당에 온걸 환영해 연진아</h1>
+<nav class="navBar">
+<ul class="menu">
+  
+  <li><a href="#">월요일 학식</a></li>
+  <li><a href="#">화요일 학식</a></li>
+  <li><a href="#">수요일 학식</a></li>
+  <li><a href="#">목요일 학식</a></li>
+  <li><a href="#">금요일 학식</a></li>
+
+</ul>
+<a href="#" class="burger"><i class="fa-solid fa-burger"></i></a> 
+</nav>
+
+    <footer class = "FooterClass">
+      <ul class = FooterUl>
+        <li class = "LiF"><a href="#">돈까스</a></li>
+        <li class = "LiF"><a href="#">김치볶음밥</a></li>
+        <li class = "LiF"><a href="#">라면</a></li>
+        <li class = "LiF"><a href="#">짬뽕</a></li>
+        <li class = "LiF"><a href="#">브리또</a></li>
+      </ul>
+    </footer>
+
+
+`;
